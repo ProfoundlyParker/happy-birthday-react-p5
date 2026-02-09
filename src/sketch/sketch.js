630af53fa9b4ae2dd242cea94c56d2bb2039af80
ProfoundlyParker <parkerramey@yahoo.com>
2026-02-09 01:03:38 -0500

    Improve canvas resizing logic and ensure proper handling of mobile view

--- src/sketch/sketch.js
+++ src/sketch/sketch.js
@@ -10,6 +10,9 @@ export default (parent, text) => (sketch) => {
   let firstWordPoints;
   let secondWordPoints;
   let thirdWordPoints = [];
+  let resizeTimeout;
+  let lastWidth;
+  let lastHeight;
 
   sketch.preload = () => {
     font = sketch.loadFont(fontFile);
@@ -28,6 +31,7 @@ export default (parent, text) => (sketch) => {
       // The numbers would be different for different text, any suggestions about better way to do this are welcome :) 
       firstWordPoints = font.textToPoints(`${firstWord} ${secondWord}`, width * 0.07, height * 0.33, width * 0.12, { sampleFactor: 0.15 });
       secondWordPoints = font.textToPoints(thirdWord, width * 0.05, height * 0.83, width * 0.25, { sampleFactor: 0.15 });
+      thirdWordPoints = [];
     } else {
       // console.log('in mobile view')
 
@@ -52,6 +56,8 @@ export default (parent, text) => (sketch) => {
   sketch.setup = () => {
     const width = parent.offsetWidth;
     const height = parent.offsetHeight;
+    lastWidth = width;
+    lastHeight = height;
     sketch.createCanvas(width, height);
     fillDots(width, height);
     sketch.frameRate(defaultFrameRate);
@@ -67,14 +73,18 @@ export default (parent, text) => (sketch) => {
   };
 
   sketch.windowResized = () => {
-    // Use setTimeout to ensure parent has resized before updating canvas
-    setTimeout(() => {
+    // Debounce resize to prevent scrolling from triggering recalculation on mobile
+    clearTimeout(resizeTimeout);
+    resizeTimeout = setTimeout(() => {
       const width = parent.offsetWidth;
       const height = parent.offsetHeight;
-      if (width > 0 && height > 0) {
+      // Only resize if dimensions actually changed
+      if (width !== lastWidth || height !== lastHeight) {
+        lastWidth = width;
+        lastHeight = height;
         sketch.resizeCanvas(width, height);
         fillDots(width, height);
       }
-    }, 0);
+    }, 250);
   };
 };
